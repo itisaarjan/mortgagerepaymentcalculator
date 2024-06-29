241938f597fce43714d2e3bb2e3fca387294729c
itisaarjan <arjansubedi@usf.edu>
2024-06-29 17:49:08 -0400

    End of Day 2

---
+++ script.js
@@ -0,0 +1,27 @@
+<div class="input-group group1">
+                    <label for="mortgage-amount">Mortgage Amount</label>
+                    <input type="text" id="mortgage-amount">
+                </div>
+                <div class="input-group group2">
+                    <div class="input-group-2a group2-inner">
+                        <label for="mortgage-term">Mortgage Term</label>
+                        <input type="text" id="mortgage-term">
+                    </div>
+                    
+                    <div class="input-group-2b group2-inner">
+                        <label for="interest-rate">Interest Rate</label>
+                        <input type="text" id="interest-rate">
+                    </div>
+                </div>
+                
+                <div class="input-group group3">
+                    <div class="radio-group">
+                        <input type="radio" name="payment_type" id="repayments">
+                        <label for="repayments">Repayments</label>
+                    </div>
+                    <div class="radio-group">
+                        <input type="radio" name="payment_type" id="interest-only">
+                        <label for="interest-only">Interest Only</label>
+                    </div>
+                </div>
+                <div class="input-group"></div>
